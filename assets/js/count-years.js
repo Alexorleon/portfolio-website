804e286f64c0de5f199ea63569958227d8b11003
Alex Esipenko <alexorleon@gmail.com>
2025-03-30 01:04:14 +0700

    Clear project. First step

---
+++ assets/js/count-years.js
@@ -0,0 +1,14 @@
+const startDate = new Date(1989, 4, 25);
+        
+function calculateYears() {
+    const now = new Date();
+    const diffInMs = now - startDate;
+    const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+    
+    return Math.floor(diffInYears);
+}
+
+window.onload = function() {
+    const years = calculateYears();
+    document.getElementById('yearsCount').textContent = years;
+};
